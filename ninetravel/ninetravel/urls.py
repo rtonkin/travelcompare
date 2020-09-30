148a3bea67fab80d262d906776c82eeb6f2ce742
Robert Tonkin <robertjtonkin@hotmail.com>
2020-09-30 20:59:30 +0930

    Initial commit

---
+++ ninetravel/ninetravel/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('travelsites/<product>/<sitea>-vs-<siteb>/', views.sitecomparison, name='comparison'),
+    path('travelsites/features/<slug>/', views.featurecomparison, name='feature'),
+    path('travelsites/<product>/<slug>/', views.site, name='site'),
+    path('articles/all/', views.allarticles, name='allarticles'),
+    path('articles/<slug>/', views.article, name='article'),
+    path('about/', views.about, name='about')
+]
