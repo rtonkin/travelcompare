93296b4581593c090e97205202bd2f9ec2816d89
Robert Tonkin <robertjtonkin@hotmail.com>
2020-11-26 22:14:26 +1030

    Add get asbolute url to siteproducts model

--- main/models.py
+++ main/models.py
@@ -86,7 +86,7 @@ class SiteProduct(models.Model):
     link = models.CharField(max_length=200)
 
     def get_absolute_url(self):
-        return '/travelsites/' + self.product.name.lower() + '/' + self.name.lower() + '/'
+        return '/travelsites/' + self.product.name.lower() + '/' + self.site.name.lower() + '/'
 
     def __str__(self):
         name = self.site.name + " " + self.product.name
